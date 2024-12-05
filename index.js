2a8f86695f08c7397036e6f24446cefa8376a490
KidHarlesh <alowoesintoluwalase@gmail.com>
2024-12-05 06:26:42 -0800

    undersatnding more

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-
 // fecthing data from api uisng javascript
-// fetch
-function getadvice() {
-  let heading = document.getElementById("freeadvice");
-  fetch("https://api.adviceslip.com/advice").then((res) =>
-    res.json().then((data) => {
-      console.log(data);
-      heading.innerHTML = data.slip.advice;
-    })
-  );
-}
 
-
+// function getadvice() {
+//   let heading = document.getElementById("freeadvice");
+//   fetch("https://api.adviceslip.com/advice").then((res)=>
+//     res.json().then((data) => {
+//       console.log(data);
+//       heading.innerHTML = data.slip.advice;
+//     })
+//   );
+// }
 // getadvice();
-async function getadvice(){
 
-  try{
+async function getadvice() {
+  try {
     let heading = document.getElementById("freeadvice");
-    let response= await fetch("https://api.adviceslip.com/advice");
-    let data =await response.json();
+    let response = await fetch("https://api.adviceslip.com/advice");
+    let data = await response.json();
     let newData = data.slip.advice;
     console.log(newData);
     heading.innerHTML = newData;
-  }
-  catch(error){
-
-  }
+  } catch (error) {}
 }
+
+
+
+
+
